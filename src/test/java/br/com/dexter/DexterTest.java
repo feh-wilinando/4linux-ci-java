497f022836fac99e9494b58684dfdb758536e80e
gustavo <gustavo@4linux.com.br>
2017-11-25 09:17:05 -0200

    Ajustando testes end to end

--- src/test/java/br/com/dexter/DexterTest.java
+++ src/test/java/br/com/dexter/DexterTest.java
@@ -34,7 +34,7 @@ public class DexterTest {
 
   @After
   public void tearDown() throws Exception {
-    driver.quit();
+//    driver.quit();
     String verificationErrorString = verificationErrors.toString();
     if (!"".equals(verificationErrorString)) {
       fail(verificationErrorString);
