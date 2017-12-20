4a81222aef2a5e7c4333bf92ab5e98b066ecd6ab
curso <curso@4linux.com.br>
2017-12-20 14:26:13 -0200

    Update CodigoZuado.java

--- src/main/java/br/com/dexter/business/CodigoZuado.java
+++ src/main/java/br/com/dexter/business/CodigoZuado.java
@@ -6,27 +6,29 @@ package br.com.dexter.business;
  * Classe que precisa logar as ações que são executadas
  */
 public class CodigoZuado {
-	
+
+    private static final String INSTANCIANDO = "Iniciando requisição";
+    	
 	public CodigoZuado() {
 		System.out.println("Instanciando a classe");
 	}
 
 	public void metodoA() {
-		System.out.println("Iniciando requisição");
+		System.out.println(INSTANCIANDO);
 		System.out.println("Dentro do método A");
 		System.out.println("Fim do método A");
 
 	}
 
 	public void metodoB() {
-		System.out.println("Iniciando requisição");
+		System.out.println(INSTANCIANDO);
 		System.out.println("Dentro do método B");
 		System.out.println("Fim do método B");
 
 	}
 
 	public void metodoC() {
-		System.out.println("Iniciando requisição");
+		System.out.println(INSTANCIANDO);
 		System.out.println("Dentro do método C");
 		System.out.println("Fim do método C");
 
