0fd33e8ae8c8350d7d5b523840380791fb048c43
curso <curso@4linux.com.br>
2017-12-20 17:39:15 -0200

    Update DexterTest.java

--- src/test/java/br/com/dexter/DexterTest.java
+++ src/test/java/br/com/dexter/DexterTest.java
@@ -1,13 +1,13 @@
 package br.com.dexter;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import org.junit.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+// import java.util.regex.Pattern;
+// import java.util.concurrent.TimeUnit;
+// import org.junit.*;
+// import static org.junit.Assert.*;
+// import static org.hamcrest.CoreMatchers.*;
+// import org.openqa.selenium.*;
+// import org.openqa.selenium.firefox.FirefoxDriver;
+// import org.openqa.selenium.support.ui.Select;
 
 public class DexterTest {
 //  private WebDriver driver;
