7308716185c9f7bd3c5189f3013c6c236b578854
Fernando Willian <feh.wilinando@gmail.com>
2017-12-21 10:13:16 -0200

    Removendo imports desnecessarios

--- src/test/java/br/com/dexter/DexterTest.java
+++ src/test/java/br/com/dexter/DexterTest.java
@@ -5,9 +5,9 @@ import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+//import org.openqa.selenium.*;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.support.ui.Select;
 
 public class DexterTest {
 //  private WebDriver driver;
